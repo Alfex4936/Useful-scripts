141a9eb00fc30ccb7835210ce004ec1ab4dbda08
franco-c <tjrdnjs33936@gmail.com>
2013-05-17 16:30:26 +0900

    Shit go back

--- linaro.sh
+++ linaro.sh
@@ -1,1 +1,1 @@
-mka liquid TARGET_TOOLS_PREFIX=`pwd`/android-toolchain-eabi/bin/arm-linux-androideabi- TARGET_PRODUCT=liquid_maguro
+mka bacon TARGET_TOOLS_PREFIX=`pwd`/android-toolchain-eabi/bin/arm-linux-androideabi- TARGET_PRODUCT=mirage_$1
