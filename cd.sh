a3d0cf175603be9630e581af401b6f58034a9629
franco-c <tjrdnjs33936@gmail.com>
2013-05-19 21:44:35 +0900

    cd.sh: Cleaner and Better

--- cd.sh
+++ cd.sh
@@ -5,21 +5,26 @@ APPS="$1"
 
 if [ -z "$APPS" ]; then
         echo -e $CL_RED"Please, Set APP where you are gonna moving"$CL_RST
-        cd packages/apps
-fi
-
-if [ "$APPS" == "mmms" ] || [ "$APPS" == "MMS" ]; then
-        echo -e "Moving to "$CL_YLW""packages/apps/Mms""$CL_RST
-        cd packages/apps/Mms
+        cd packages/apps 2>/dev/null
 fi
 
 if [ "$APPS" == "settings" ] || [ "$APPS" == "Setings" ] || [ "$APPS" == "Settings" ]
     then
         echo -e "Moving to "$CL_YLW""packages/apps/Settings""$CL_RST
-        cd packages/apps/Settings
-fi
-
-if [ ! "$APPS" == "settings" ] || [ ! "$APPS" == "Setings" ] || [ ! "$APPS" == "Settings" ] || [ ! "$APPS" == "MMS" ] || [ ! "$APPS" == "mms" ]; then
+        cd packages/apps/Settings 2>/dev/null
+else
+    if [ "$APPS" == "mms" ] || [ "$APPS" == "MMS" ]; then
+        echo -e "Moving to "$CL_MAG""packages/apps/Mms""$CL_RST
+        cd packages/apps/Mms 2>/dev/null
+else
+    if [ ! -d packages/apps/$1 ]
+    then
+        echo -e $CL_RED"There's no such a folder like "$1""$CL_RST
+        echo -e $CL_RED"Set your destination again"$CL_RST
+        cd packages/apps
+else
         echo -e "Moving to "$CL_CYN""packages/apps/$1""$CL_RST
-        cd packages/apps/$1
+        cd packages/apps/$1 2>/dev/null
+    fi
+  fi
 fi
